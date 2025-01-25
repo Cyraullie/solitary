5502fc2e95bfe8a59dd9cfb8b0a5720238004925
cyraullie <cyril.goldenschue@gmail.com>
2025-01-25 02:34:57 +0100

    add many include

---
+++ solitary.h
@@ -0,0 +1,13 @@
+#ifndef SOLITARY_H
+# define SOLITARY_H
+
+# include "mlx_linux/mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+#endif
