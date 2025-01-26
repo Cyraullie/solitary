9a2061c9fec408f70221c72f5cf1147bcd11f451
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-26 12:06:20 +0100

    use structur var

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:45:09 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/26 11:52:02 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/26 12:05:06 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 
 int	main(void)
 {
-	void	*mlx;
-	void	*win;
+	t_var	var;
 
-	mlx = mlx_init();
-	if (!mlx)
+	var.mlx = mlx_init();
+	if (!var.mlx)
 	{
 		printf("Failed to initialize MLX\n");
 		return (1);
 	}
 	printf("MLX initialized successfully!\n");
-	win = mlx_new_window(mlx, 800, 600, "MLX Test");
-	if (!win)
+	var.win = mlx_new_window(var.mlx, 800, 600, "MLX Test");
+	if (!var.win)
 	{
 		fprintf(stderr, "Failed to create a window\n");
 		return (1);
 	}
-	mlx_loop(mlx);
+	mlx_loop(var.mlx);
 	return (0);
 }
